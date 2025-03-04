aaf75a0f5262750a96af4c3f39a933adf70e04a5
Vali <valireyimbergenov79@gmail.com>
2025-03-04 20:08:41 +0500

    Add contactService

--- src/contacts/contacts.service.ts
+++ src/contacts/contacts.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Contact } from "./entities/contact.entity";
 import { Repository } from "typeorm";
 import { CreateContactDto } from "./dto/create-contact.dto";
+import { UpdateContactDto } from "./dto/update-contact.dto";
 
 
 @Injectable()
@@ -12,9 +13,33 @@ export class ContactsService{
     private readonly contactRepository: Repository<Contact>,
   ){}
 
-  async create(CreateContactDto: CreateContactDto): Promise<Contact>{
-    const contact = this.contactRepository.create(CreateContactDto);
-
+  async create(createContactDto: CreateContactDto): Promise<Contact>{
+    const contact = this.contactRepository.create(createContactDto);
     return this.contactRepository.save(contact)
+  }
+
+  async findAll(id: number): Promise<Contact[]>{
+    return this.contactRepository.find({
+      order: {name: "ASC"},
+    });
+  }
+
+  async findOne(id: number): Promise<Contact>{
+    const contact = await this.contactRepository.findOne({where: {id}});
+    if(!contact){
+      throw new NotFoundException(`Contact with ID ${id} not found`);
+    }
+    return contact;
+  }
+
+  async update(id: number, UpdateContactDto: UpdateContactDto): Promise<Contact>{
+    const contact = await this.findOne(id);
+    Object.assign(contact, UpdateContactDto);
+    return this.contactRepository.save(contact);
+  }
+
+  async remove(id: number): Promise<void>{
+    const contact = await this.findOne(id);
+    await this.contactRepository.remove(contact);
   }
 }
